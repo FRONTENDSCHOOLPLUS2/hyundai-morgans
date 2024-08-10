bf11b98b8b9b4eeb578a7b84ad367352f82c865b
redcontroller <jasinis102@gmail.com>
2024-08-10 14:50:15 +0900

    :lipstick: fix: #5 header 컴포넌트 수정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,39 +1,32 @@
 import { auth } from "@/auth";
-import HeaderChild from "./HeaderChild";
 
-export default async function Header() {
+export default async function Header({ isMain }: {isMain:string}) {
   const session = await auth();
   console.log('session', session);
 
   return (
-      <header>
-<<<<<<< Updated upstream
-        <h1 className="gnb_logo">
-          <a href="#none"><img src="/images/genisisu_logo_w.png" alt={'타이틀이미지'}/> </a>
-        </h1>
-        <ul className="gnb">
-          <li>
-            <ul>
-              <li><a href="/models">모델</a></li>
-              <li><a href="#none">전시시승</a></li>
-              <li><a href="#none">고객지원</a></li>
-              <li><a href="#none">공지사항</a></li>
-              <li><a href="#none">재니시수연</a></li>
+        <header className={isMain} >
+            <h1 className="gnb_logo">
+                <a href="/"><img src="/images/genisisu_logo_w.png" alt={'타이틀이미지'}/> </a>
+            </h1>
+            <ul className="gnb">
+                <li>
+                <ul>
+                    <li><a href="/models">모델</a></li>
+                    <li><a href="#none">전시시승</a></li>
+                    <li><a href="#none">고객지원</a></li>
+                    <li><a href="#none">공지사항</a></li>
+                    <li><a href="#none">재니시수연</a></li>
+                </ul>
+                </li>
+                <li>
+                <ul>
+                    <li><a href="#none">로그인</a></li>
+                    <li><a href="#none">회원가입</a></li>
+                    <li><a href="#none"><img src="/images/menu_ham.png" alt=""/></a></li>
+                </ul>
+                </li>
             </ul>
-          </li>
-          <li>
-            <ul>
-              <li><a href="#none">로그인</a></li>
-              <li><a href="#none">회원가입</a></li>
-              <li><a href="#none"><img src="/images/menu_ham.png" alt=""/></a></li>
-            </ul>
-          </li>
-        </ul>
-=======
-        <HeaderChild/>
->>>>>>> Stashed changes
-      </header>
-
-
+        </header>
   );
 }
