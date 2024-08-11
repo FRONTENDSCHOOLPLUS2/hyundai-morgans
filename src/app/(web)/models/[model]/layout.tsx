d4263cc9ba279eebe277b6d061473a421966a535
류재준.주임연구원 <ryungom@ezpmp.co.kr>
2024-08-12 00:05:59 +0900

    :sparkles:feat:#28 첫번째섹션 퍼블리싱 완료

---
+++ src/app/(web)/models/[model]/layout.tsx
@@ -0,0 +1,21 @@
+import ModelGnb from "./ModelGnb";
+import ModelLnb from "./ModelLnb";
+
+
+export default function modelLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+
+  
+
+  return (
+    <main className="text-white relative">
+      {/* 상단 가로 드롭다운 메뉴 */}
+      <ModelGnb/>
+
+      {/* 좌측 LNB */}
+      <ModelLnb/>
+
+      {/* 상세보기 ~ 결제하기까지 레이아웃 변경점 */}
+      {children}
+    </main>
+  );
+}
--- src/app/(web)/models/[model]/page.tsx
+++ src/app/(web)/models/[model]/page.tsx
@@ -1,91 +1,39 @@
-export default function OrderPage () {
-    return (
-        <main className="text-white">
+export default function OrderPage() {
+  return (
+    <>
+      {/* 첫번째 섹션 : 360도 이미지 */}
+      <section className="min-h-screen bg-black relative">
+        {/* 이미지 영역 */}
+        <article className="absolute top-[80px] right-[85px] w-[calc(100vw-300px)] grid grid-cols-1 justify-items-center">
+          <figure className="max-h-[500px] overflow-hidden">
+            <img src="/images/detail/defaultCar.png" className="object-cover h-[100%] scale-150 " alt="" />
+          </figure>
+          <h2 className="text-[40px] font-Hyundai-sans">G90 Black</h2>
+          <h3 className="text-[30px] text-[#a4a4a4]">
+            시작가격 <span className="text-white font-Hyundai-sans">123,123,123</span>
+            <span className="text-[25px] text-white">원</span>
+          </h3>
+          <div className="text-[30px] flex gap-x-[60px] mt-[20px]">
+            <button className="w-[320px] h-[70px]">시승신청</button>
+            <button className="w-[320px] h-[70px] text-black bg-white">다음</button>
+          </div>
+        </article>
+      </section>
 
-            {/* 첫번째 섹션 */}
-            <section className="min-h-screen bg-black relative">
+      {/* 두번째 섹션 : 동영상 or 이미지 fixed 후 text 스크롤링 */}
+      <section>두번째섹션</section>
 
-              {/* 상단 가로 드롭다운 메뉴 */}
-              <nav className="flex items-end absolute top-[15px] left-[85px]">
-                <button className="grid col-auto font-Hyundai-sans w-[230px] h-[70px] pt-[8px] px-[20px]">
-                  <span className="col-start-1 text-[13px] self-end text-left ">GENISISUYEON</span>
-                  <span className="col-start-1 text-[22px] self-start text-left font-black">G90 Black</span>
-                  <div className="col-start-2 row-start-1 row-span-2 self-center justify-self-end w-[10px] h-[20px]">
-                    <img src="/images/btn_next.png" className="object-cover" alt="" />
-                  </div>
-                </button>
-                <ul className="flex">
-                  <li>
-                    <a href="#none" className="grid col-auto font-Hyundai-sans w-[150px] h-[50px] pt-[8px] px-[20px] border-[#777] border-[1px]">
-                      <span className="col-start-1 text-[10px] self-end text-left ">GENISISUYEON</span>
-                      <span className="col-start-1 text-[17px] self-start text-left font-medium">G90 Black</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#none" className="grid col-auto font-Hyundai-sans w-[150px] h-[50px] pt-[8px] px-[20px] border-[#777] border-[1px]">
-                      <span className="col-start-1 text-[10px] self-end text-left ">GENISISUYEON</span>
-                      <span className="col-start-1 text-[17px] self-start text-left font-medium">G90 Black</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#none" className="grid col-auto font-Hyundai-sans w-[150px] h-[50px] pt-[8px] px-[20px] border-[#777] border-[1px]">
-                      <span className="col-start-1 text-[10px] self-end text-left ">GENISISUYEON</span>
-                      <span className="col-start-1 text-[17px] self-start text-left font-medium">G90 Black</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#none" className="grid col-auto font-Hyundai-sans w-[150px] h-[50px] pt-[8px] px-[20px] border-[#777] border-[1px]">
-                      <span className="col-start-1 text-[10px] self-end text-left ">GENISISUYEON</span>
-                      <span className="col-start-1 text-[17px] self-start text-left font-medium">G90 Black</span>
-                    </a>
-                  </li>
-                </ul>
-              </nav>
+      {/* 세번째 섹션 : 색상선택 옵션 */}
+      <section className="min-h-screen bg-black">세번째섹션</section>
 
-              {/* 좌측 LNB */}
-              <ul>
-                <li><a href="#none">모델 상세</a></li>
-                ``
-              </ul>
+      {/* 네번째 섹션 : 외장디자인 설명 */}
+      <section>네번쩨 섹션</section>
 
-              {/* 이미지 영역 */}
-              <article>
-                <h2>G90 Black</h2>
-                <h3>시작가격 <span>123,123,123</span><span>원</span></h3>
-                <div>
-                  <button>시승신청</button>
-                  <button>다음</button>
-                </div>
-              </article>
-              <figure className="absolute inset-0">
-                  <img src="/images/detail/genesis_detail_360samp01.jpg" className="object-cover" alt="" />
-                </figure>
-            </section> 
+      {/* 다섯번재 섹션 : 내장디자인 설명 */}
+      <section>다섯번째 섹션</section>
 
-            {/* 두번째 섹션 : 동영상 or 이미지 fixed 후 text 스크롤링 */}
-            <section>
-              두번째섹션
-            </section>
-
-            {/* 세번째 섹션 : 색상선택 옵션 */}
-            <section className="min-h-screen bg-black">
-              세번째섹션
-            </section>
-
-            {/* 네번째 섹션 : 외장디자인 설명 */}
-            <section>
-              네번쩨 섹션
-            </section>
-
-            {/* 다섯번재 섹션 : 내장디자인 설명 */}
-            <section>
-              다섯번째 섹션
-            </section>
-
-            {/* 여섯번째 섹션(마지막) : 테이블 스펙 */}
-            <section>
-              마지막 섹션
-            </section>
-        </main>
-    );
+      {/* 여섯번째 섹션(마지막) : 테이블 스펙 */}
+      <section>마지막 섹션</section>
+    </>
+  );
 }
