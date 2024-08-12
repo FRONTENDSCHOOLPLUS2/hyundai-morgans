680b666d08debfed4bd027b3646468b215449da5
류재준 <ryungom@gmail.com>
2024-08-12 12:25:34 +0900

    :lipstick:style:#28: 컴플릭트 오류 수정

--- src/app/(web)/models/[model]/page.tsx
+++ src/app/(web)/models/[model]/page.tsx
@@ -1,6 +1,30 @@
-export default function OrderPage() {
+import { fetchProduct } from "@/data/fetch/productFetch";
+import { ImageViewer } from "./ImageViewer";
+
+const SERVER = process.env.NEXT_PUBLIC_API_SERVER;
+
+export default async function OrderPage() {
+
+
+  const data = await fetchProduct("1");
+  let imageArray:string[] = [];
+
+  if (data) {
+      imageArray = data?.extra.detail.view360Images.map(image => SERVER + image.path);
+      // console.log(imageArray);
+  }
+
+
   return (
     <>
+
+        {/* <main className="bg-black">
+            { data && <ImageViewer images={imageArray} />}
+        </main> */}
+
+
+
+
       {/* 첫번째 섹션 : 360도 이미지 */}
       <section className="min-h-screen bg-black relative z-10">
         {/* 이미지 영역 */}
