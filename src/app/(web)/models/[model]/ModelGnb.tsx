74afe17243f0ab79badb79455cc0e5ca7a7164dc
류재준 <ryungom@gmail.com>
2024-08-12 13:16:54 +0900

    :lipstick:style#29 레이아웃 위치 미세 수정

--- src/app/(web)/models/[model]/ModelGnb.tsx
+++ src/app/(web)/models/[model]/ModelGnb.tsx
@@ -2,77 +2,41 @@
 
 import { useModelStore } from "@/zustand/useModel";
 
-export default function ModelGnb({ params }: { params: { model: string } }) {
-  const { items } = useModelStore();
-  const modelName = items[Number(params.model) - 1].toUpperCase();
-
-  return (
-    <nav className="flex items-end absolute top-[40px] left-[80px] z-20">
-      <button className="grid col-auto font-Hyundai-sans w-[300px] h-[70px] pt-[8px] px-[20px] bg-black">
-        <span className="col-start-1 text-[13px] self-end text-left ">
-          GENISISUYEON
-        </span>
-        <span className="col-start-1 text-[22px] self-start text-left font-black">
-          {modelName}
-        </span>
-        <div className="col-start-2 row-start-1 row-span-2 self-center justify-self-end w-[10px] h-[20px]">
-          <img src="/images/btn_next.png" className="object-cover" alt="" />
-        </div>
-      </button>
-      <ul className="flex">
-        <li>
-          <a
-            href="#none"
-            className="grid col-auto font-Hyundai-sans w-[200px] h-[70px] pt-[8px] px-[20px] border-[#777] border-[1px] bg-black"
-          >
-            <span className="col-start-1 text-[10px] self-end text-left ">
-              GENISISUYEON
-            </span>
-            <span className="col-start-1 text-[17px] self-start text-left font-medium">
-              {modelName}
-            </span>
-          </a>
-        </li>
-        <li>
-          <a
-            href="#none"
-            className="grid col-auto font-Hyundai-sans w-[200px] h-[70px] pt-[8px] px-[20px] border-[#777] border-[1px] bg-black"
-          >
-            <span className="col-start-1 text-[10px] self-end text-left ">
-              GENISISUYEON
-            </span>
-            <span className="col-start-1 text-[17px] self-start text-left font-medium">
-              {modelName}
-            </span>
-          </a>
-        </li>
-        <li>
-          <a
-            href="#none"
-            className="grid col-auto font-Hyundai-sans w-[200px] h-[70px] pt-[8px] px-[20px] border-[#777] border-[1px] bg-black"
-          >
-            <span className="col-start-1 text-[10px] self-end text-left ">
-              GENISISUYEON
-            </span>
-            <span className="col-start-1 text-[17px] self-start text-left font-medium">
-              {modelName}
-            </span>
-          </a>
-        </li>
-        <li>
-          <a
-            href="#none"
-            className="grid col-auto font-Hyundai-sans w-[200px] h-[70px] pt-[8px] px-[20px] border-[#777] border-[1px] bg-black"
-          >
-            <span className="col-start-1 text-[10px] self-end text-left ">
-              GENISISUYEON
-            </span>
-            <span className="col-start-1 text-[17px] self-start text-left font-medium">
-              {modelName}
-            </span>
-          </a>
-        </li>
-      </ul>
-    </nav>
-  );
+  return(
+      <nav className="flex items-end absolute top-[40px] left-[80px] z-20" >
+        <button  className="grid col-auto font-Hyundai-sans w-[230px] h-[70px] pt-[8px] px-[20px]">
+          <span className="col-start-1 text-[13px] self-end text-left ">GENISISUYEON</span>
+          <span className="col-start-1 text-[22px] self-start text-left font-black">G90 Black</span>
+          <div className="col-start-2 row-start-1 row-span-2 self-center justify-self-end w-[10px] h-[20px]">
+            <img src="/images/btn_next.png" className="object-cover" alt="" />
+          </div>
+        </button>
+        <ul className="flex">
+          <li>
+            <a href="#none" className="grid col-auto font-Hyundai-sans w-[150px] h-[50px] pt-[8px] px-[20px] border-[#777] border-[1px]">
+              <span className="col-start-1 text-[10px] self-end text-left ">GENISISUYEON</span>
+              <span className="col-start-1 text-[17px] self-start text-left font-medium">G90 Black</span>
+            </a>
+          </li>
+          <li>
+            <a href="#none" className="grid col-auto font-Hyundai-sans w-[150px] h-[50px] pt-[8px] px-[20px] border-[#777] border-[1px]">
+              <span className="col-start-1 text-[10px] self-end text-left ">GENISISUYEON</span>
+              <span className="col-start-1 text-[17px] self-start text-left font-medium">G90 Black</span>
+            </a>
+          </li>
+          <li>
+            <a href="#none" className="grid col-auto font-Hyundai-sans w-[150px] h-[50px] pt-[8px] px-[20px] border-[#777] border-[1px]">
+              <span className="col-start-1 text-[10px] self-end text-left ">GENISISUYEON</span>
+              <span className="col-start-1 text-[17px] self-start text-left font-medium">G90 Black</span>
+            </a>
+          </li>
+          <li>
+            <a href="#none" className="grid col-auto font-Hyundai-sans w-[150px] h-[50px] pt-[8px] px-[20px] border-[#777] border-[1px]">
+              <span className="col-start-1 text-[10px] self-end text-left ">GENISISUYEON</span>
+              <span className="col-start-1 text-[17px] self-start text-left font-medium">G90 Black</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+  )
 }
