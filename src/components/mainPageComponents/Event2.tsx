adb5541621992ba9f6d2383550035e4f9736e4a1
류재준 <ryungom@gmail.com>
2024-08-18 15:26:58 +0900

    :sparkles:feat:#45 메인페이지 두번째 섹션 캐러셀 작업

--- src/components/mainPageComponents/Event2.tsx
+++ src/components/mainPageComponents/Event2.tsx
@@ -1,17 +1,36 @@
 'use client'
-
-import { fetchProducts } from "@/data/fetch/productFetch";
+import { Product } from "@/types/product";
 import Image from "next/image";
 import { useState } from "react";
+const SERVER : string = process.env.NEXT_PUBLIC_API_SERVER;
 
 
-export default function Event2 ( {data}) {
-
-  console.log("확인::::",data)
-
+export default function Event2 ( {data} : {data:Product}) {
   const [index, setIndex] = useState(0);
+
+  const imageData: React.ReactNode[] = data.map((img) => (
+    <figure key={img.name}>
+      <Image src={SERVER + img.mainImages[0].path} width={0} height={0} sizes="100%" alt="G70 car" />
+    </figure>
+  ))
+  const imgLength = imageData.length
+  const nameData = data.map(modelName => modelName.name.split('-'))
+  // const modelId = data.map(modelId => modelId._id)
+
+  const subtitle = nameData.map(modelName => {
+    if (modelName.length >1 ) {
+      return modelName.slice(1,4).join(' ');
+    }
+    return 'STANDARD';
+  })
+
   const handleIndexNumPlus = () => {
-    setIndex((prev) => prev + 1)
+    setIndex((prev) => {
+      if (prev < imgLength -1) {
+        return prev + 1
+      }
+      return prev = 0;
+    })
   }
   const handleIndexNumMinus = () => {
     setIndex((prev) => {
@@ -22,29 +41,24 @@ export default function Event2 ( {data}) {
     });
   }
 
-
-
   return(
     <section id="event2">
         <article>
-          <h2>G70</h2>
-          <h3>STANDARD</h3>
-          <button className="mainBtn">VIEW MORE</button>
+          <h2>{nameData[index][0].toUpperCase()} </h2>
+          <h3>{subtitle[index].toUpperCase()}</h3>
         </article>
         <section>
           <span className="ev2_prev" onClick={handleIndexNumMinus}></span>
           <div className="ev2_wrap">
-            <figure style={{left:`${-100 * index }%`}}>
-              <Image src="/images/g70.png" width={0} height={0} sizes="100%" alt="G70 car" />
-              <Image src="/images/g90.png" width={0} height={0} sizes="100%" alt="G70 car" />
-            </figure>
+            <article style={{left:`${-100 * index}%`, width:`${100 * imgLength}%`}}>
+                {data && imageData}
+            </article>
           </div>
-          
           <span className="ev2_next" onClick={handleIndexNumPlus}></span>
         </section>
         
         <div className="ev2_bg">
-          
+          <button className="mainBtn">VIEW MORE</button>
         </div>
 
       </section>
