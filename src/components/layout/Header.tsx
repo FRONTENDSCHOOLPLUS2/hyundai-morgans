9bc8fe25fcf93eefc9d85073693c0e58e9e2bf05
류재준 <ryungom@gmail.com>
2024-08-23 22:01:16 +0900

    ✨feat:#100 메인페이지 Header SITE MAP 개발

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,61 @@
+'use client'
+
 // import { auth } from "@/auth";
 import Link from 'next/link';
+import { useEffect, useRef, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Header({ isMain }: { isMain: string }) {
   // export default async function Header({ isMain }: {isMain:string}) {
   // const session = await auth();
   // console.log('session', session);
+
+  const modelRef = useRef(null)
+  const route = useRouter();
+  const [modelOn, setModelOn] = useState(true)
+  const handleSiteMapOpen = (e: React.MouseEvent<HTMLElement>) : void => {
+    e.preventDefault();
+    setModelOn(!modelOn)
+  }
+  const handleCloseBtn = () => {
+    setModelOn(!modelOn)
+  }
+
+  const handleSitemapClick = (e: React.MouseEvent<HTMLElement>,index:number) : void => {
+    e.preventDefault();
+    if (index < 13) {
+      route.push(`/models/${index}`)
+    } else if (index === 13) {
+      alert('아직 개발중이에요')
+    } else if (index > 13) {
+      switch(index) {
+        case 14:
+          route.push('/info')
+          break;
+        default:
+          route.push('#')
+      }
+    }
+    
+    setModelOn(!modelOn)
+  }
+
+  useEffect(()=>{
+    const htmlBody = document.querySelector('body') as HTMLBodyElement
+    if (!modelOn) {
+      const scrollY = window.scrollY;
+      document.body.style.top = `-${scrollY}px`;
+      document.body.style.position = 'fixed';
+      htmlBody.classList.toggle('modalOn')
+    } else {
+      const scrollY = document.body.style.top;
+      document.body.style.top = '';
+      document.body.style.position = '';
+      htmlBody.classList.toggle('modalOn')
+      window.scrollTo(0, parseInt(scrollY || '0') * -1);
+
+    }
+  },[modelOn])
 
   return (
     <header className={isMain}>
@@ -43,13 +94,65 @@ export default function Header({ isMain }: { isMain: string }) {
               <Link href="/signup">회원가입</Link>
             </li>
             <li>
-              <Link href="#none">
+              <Link href="#" onClick={(e) => handleSiteMapOpen(e)}>
                 <img src="/images/menu_ham.png" alt="" />
               </Link>
             </li>
           </ul>
         </li>
       </ul>
+        <div className={["modal", modelOn ? "" : "on"].join(" ")} ref={modelRef}>
+          <section>
+            <h2>SITE MAP</h2>
+
+            <div className="siteItemWrap">
+              <article>
+                <h3>모델</h3>
+                <div>
+                  <Link href='#' onClick={(e) => handleSitemapClick(e,1)}>G90</Link>
+                  <Link href='#' onClick={(e) => handleSitemapClick(e,2)}>G90 LONG WHEEL BASE</Link>
+                  <Link href='#' onClick={(e) => handleSitemapClick(e,3)}>G90</Link>
+                  <Link href='#' onClick={(e) => handleSitemapClick(e,4)}>G80</Link>
+                  <Link href='#' onClick={(e) => handleSitemapClick(e,5)}>G80 ELECTRIFIED</Link>
+                  <Link href='#' onClick={(e) => handleSitemapClick(e,6)}>G70</Link>
+                  <Link href='#' onClick={(e) => handleSitemapClick(e,7)}>G70 SHOOTING BREAK</Link>
+                  <Link href='#' onClick={(e) => handleSitemapClick(e,8)}>GV80</Link>
+                  <Link href='#' onClick={(e) => handleSitemapClick(e,9)}>GV80 COUPE</Link>
+                  <Link href='#' onClick={(e) => handleSitemapClick(e,10)}>GV70</Link>
+                  <Link href='#' onClick={(e) => handleSitemapClick(e,11)}>GV70 ELECTRIFIED</Link>
+                  <Link href='#' onClick={(e) => handleSitemapClick(e,12)}>GV60</Link>
+                  <Link href='#' onClick={(e) => handleSitemapClick(e,13)}>NEOLUN CONCEPT</Link>
+                </div>
+              </article>
+
+              <article>
+                <h3>게시판</h3>
+                <div>
+                  <Link href='#' onClick={(e) => handleSitemapClick(e,14)}>전시시승</Link>
+                  <Link href='#' onClick={(e) => handleSitemapClick(e,15)}>공지사항</Link>
+                  <Link href='#' onClick={(e) => handleSitemapClick(e,16)}>어쩌구</Link>
+                  <Link href='#' onClick={(e) => handleSitemapClick(e,17)}>저쩌구</Link>
+                </div>
+              </article>
+
+              <article>
+                <h3>재니시수연</h3>
+                <div>
+                  <Link href='#' onClick={(e) => handleSitemapClick(e,18)}>김모건</Link>
+                  <Link href='#' onClick={(e) => handleSitemapClick(e,19)}>이수연</Link>
+                  <Link href='#' onClick={(e) => handleSitemapClick(e,20)}>류재준</Link>
+
+                </div>
+              </article>
+            </div>
+
+          
+          </section>
+          <div className="closeBtn" onClick={handleCloseBtn}>
+            <div className='cl-line'></div>
+            <div className='cl-line'></div>
+          </div>
+        </div>
     </header>
   );
 }
