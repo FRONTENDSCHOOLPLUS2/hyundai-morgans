eb1e18d13edd1487de3abac13602a2a8a7864317
류재준 <ryungom@gmail.com>
2024-08-23 14:33:38 +0900

    ✨feat:#99 메인페이지 event2 view more 모델페이지 연결

--- src/components/mainPageComponents/Event2.tsx
+++ src/components/mainPageComponents/Event2.tsx
@@ -1,10 +1,13 @@
 'use client'
 import { Product } from "@/types/product";
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 const SERVER : string = process.env.NEXT_PUBLIC_API_SERVER;
 
 export default function Event2 ( {data} : {data:Product[]}) {
+  const router = useRouter();
   const regex = /gv?\d{2}/g; // 정규표현식
   const [index, setIndex] = useState(0);
   const imageData = data.map((image) => (
@@ -39,6 +42,14 @@ export default function Event2 ( {data} : {data:Product[]}) {
     });
   }
 
+  const handleModelClick = () => {
+    if (index === 12) {
+      alert ("NEOLUN 차량은 준비중 입니다.\n기대해주세요")
+    } else {
+      router.push(`/models/${index + 1}`)
+    }
+  }
+
   return(
     <section id="event2">
         <article>
@@ -56,7 +67,11 @@ export default function Event2 ( {data} : {data:Product[]}) {
         </section>
         
         <div className="ev2_bg">
-          <button className="mainBtn">VIEW MORE</button>
+          <Link href="#" className="mainBtn" 
+          onClick={(e) => {
+            e.preventDefault();
+            handleModelClick();
+            }}>VIEW MORE</Link>
         </div>
       </section>
   )
