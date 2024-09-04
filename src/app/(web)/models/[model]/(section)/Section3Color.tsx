d5627691d427184e9098920435117737dfeaa20a
redcontroller <jasinis102@gmail.com>
2024-09-04 18:07:18 +0900

    :ambulance: hotfix: #142 옵션 데이터 변경에 따른 코드 수정

--- src/app/(web)/models/[model]/(section)/Section3Color.tsx
+++ src/app/(web)/models/[model]/(section)/Section3Color.tsx
@@ -1,11 +1,12 @@
-import { OptionExterior } from '@/types/product';
+import { OptionExterior, OptionItem } from '@/types/product';
 import ModelColor from '../ModelColor';
 
 interface Section3Color {
-  modelIndex: string;
-  optionData: OptionExterior | null;
+  modelName: string;
+  optionData: OptionItem[];
 }
 
-export default function Section3Color({ modelIndex, optionData }: Section3Color) {
-  return <>{optionData && <ModelColor optionData={optionData} modelIndex={modelIndex} />}</>;
+export default function Section3Color({ modelName, optionData }: Section3Color) {
+  return <ModelColor exterior={optionData} modelName={modelName} />;
 }
+
